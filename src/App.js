7bd642447b72b8898e01fe1212a0942c0538ced6
AsadiDeviSriPrasad <167798651+AsadiDeviSriPrasad@users.noreply.github.com>
2024-09-26 13:30:37 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+function App() {
+  return(
+    <>
+    <h1>This is App component</h1>
+  
+    </>
+    
+
+  )
+    
+  }
+
+
+
+export default App;
---
+++ src/Car.js
@@ -0,0 +1,6 @@
+import React from "react";
+function Car(props){
+
+    return (<h2>I am {props.brand}</h2>)
+}
+export default App;
